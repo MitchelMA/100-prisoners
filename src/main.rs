50107e17cf606295649f5d0a5436e5fe592119e5
MitchelMA <32548@ma-web.nl>
2022-06-30 22:35:39 +0200

    initial moest niet random zijn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use rand::{thread_rng, Rng};
+use rand::Rng;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
-    let boxes = random_vector(100);
-    for (i, v) in boxes.iter().enumerate() {
-        // println!("{}: {}", i, *v);
-    }
-    let outcome = prisoners_slip(boxes, 1);
+    let mut args = env::args().skip(1);
+    let stand = 10;
+    let first = match args.next() {
+        Some(v) => match v.parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => stand,
+        },
+        None => stand,
+    };
+    println!("{}", first);
+    let outcome = prisoners_slip(first as usize);
     println!("overal chance: {}%", outcome * 100.0);
 }
 
@@ -30,11 +36,14 @@ fn random_vector(length: u8) -> Vec<u8> {
     return v;
 }
 
-fn prisoners_slip(boxes: Vec<u8>, times: usize) -> f64 {
+fn prisoners_slip(times: usize) -> f64 {
+    let mut boxes;
     let mut v = Vec::<bool>::new();
+    let mut count = 0.0;
     // loop for how many times the riddle should play to get a better
     // representation of the outcome precentage
     for i in 0..times {
+        boxes = random_vector(100);
         // boolean that determines if this group of prisoners passed or not
         let mut passed = true;
         // loop to loop through all the prisoners
@@ -42,55 +51,44 @@ fn prisoners_slip(boxes: Vec<u8>, times: usize) -> f64 {
             // let the prisoner choose 50 times
             // if it is able to find its number in the boxes, it succeeds
             // but if it doesn't, everyone will die
-            // get a random initial choice
-            let mut initial_choise = thread_rng().gen_range(0..boxes.len());
-            // get the value in the initial choice
-            let mut current_box_value = boxes[initial_choise];
             // get the current prisoner
             let current_prisoner = j as u8;
+            // begin with the box that has the number of the prisoner
+            let mut current_box_value = boxes[current_prisoner as usize - 1];
             // check if this prisoner passed or not
             let mut current_pass = false;
-            for k in 0..50 {
+            for _k in 0..50 {
                 // if the value in the current box and that of the current prisoner are the same,
                 // that means that the prisoner has passed
                 if current_box_value == current_prisoner {
-                    println!(
-                        "prisoner {} found its box on choice {}",
-                        current_prisoner,
-                        k + 1
-                    );
+                    // println!(
+                    //     "prisoner {} found its box on choice {}",
+                    //     current_prisoner,
+                    //     k + 1
+                    // );
                     current_pass = true;
                     break;
                 }
 
-                // else it should get the next box
-                println!("previous value: {}", current_box_value);
+                // else it should go to the box with the labelnumber that the previous box had a slip of
                 current_box_value = boxes[current_box_value as usize - 1];
-                println!("current-value: {}", current_box_value);
-                println!("searching for: {}, on choice {}", current_prisoner, k + 1);
-
-                // make sure that the prisoner doesn't get stuck in a loop
-                println!("{} : {}", current_box_value, boxes[initial_choise]);
-                if current_box_value == boxes[initial_choise] {
-                    println!("PRISONER {} FOUND A LOOP!!!!!", current_prisoner);
-                    initial_choise = thread_rng().gen_range(0..boxes.len());
-                    current_box_value = boxes[initial_choise];
-                }
             }
             if !current_pass {
-                println!("prisoner {} didn\'t pass", current_prisoner);
+                // println!("prisoner {} didn\'t pass", current_prisoner);
                 passed = false;
                 break;
             }
         }
         v.push(passed);
-    }
-
-    let mut count = 0 as f64;
-    for i in v.iter() {
-        if *i == true {
+        if passed {
             count += 1.0;
         }
+        // calc the percentage thusfar
+        println!(
+            "chance at round {}: {}%",
+            i,
+            count / (v.len() as f64) * 100.0,
+        );
     }
 
     return count / (v.len() as f64);
