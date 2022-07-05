f6f3f9f17f9070d454d32ffdc4edd581cc20d83a
MitchelMA <32548@ma-web.nl>
2022-07-05 23:50:33 +0200

    u8 -> usize en de optie om groep-groote aan te passen

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,33 @@ use std::env;
 
 fn main() {
     let mut args = env::args().skip(1);
-    let stand = 10;
+    let rounds = 500;
+    let size = 100;
     let first = match args.next() {
         Some(v) => match v.parse::<i32>() {
             Ok(num) => num,
-            Err(_) => stand,
+            Err(_) => rounds,
         },
-        None => stand,
+        None => rounds,
     };
-    println!("{}", first);
-    let outcome = prisoners_slip(first as usize);
+    let second = match args.next() {
+        Some(v) => match v.parse::<i32>() {
+            Ok(num) => num,
+            Err(_) => size,
+        },
+        None => size,
+    };
+    println!("rounds: {}", first);
+    println!("group-size: {}", second);
+    let outcome = prisoners_slip(first as usize, second as usize);
     println!("overal chance: {}%", outcome * 100.0);
 }
 
-fn random_vector(length: u8) -> Vec<u8> {
+fn random_vector(length: usize) -> Vec<usize> {
     let mut thread_rng = rand::thread_rng();
-    let mut v = Vec::<u8>::new();
+    let mut v = Vec::<usize>::new();
     let mut index: usize = 0;
-    loop {
-        if !(index < length.into()) {
-            break;
-        }
+    while index < length {
         let r = thread_rng.gen_range(1..length + 1);
         if v.contains(&r) {
             continue;
@@ -36,7 +42,7 @@ fn random_vector(length: u8) -> Vec<u8> {
     return v;
 }
 
-fn prisoners_slip(times: usize) -> f64 {
+fn prisoners_slip(times: usize, length: usize) -> f64 {
     // vector containing the shuffled prisoner's slips
     let mut boxes;
     // vector containing all the values of passed groups
@@ -46,7 +52,7 @@ fn prisoners_slip(times: usize) -> f64 {
     // loop for how many times the riddle should play to get a better
     // representation of the outcome precentage
     for i in 0..times {
-        boxes = random_vector(100);
+        boxes = random_vector(length);
         // boolean that determines if this group of prisoners passed or not
         let mut passed = true;
         // loop to loop through all the prisoners
@@ -55,12 +61,12 @@ fn prisoners_slip(times: usize) -> f64 {
             // if it is able to find its number in the boxes, it succeeds
             // but if it doesn't, everyone will die
             // get the current prisoner
-            let current_prisoner = j as u8;
+            let current_prisoner = j;
             // begin with the box that has the number of the prisoner
             let mut current_box_value = boxes[current_prisoner as usize - 1];
             // check if this prisoner passed or not
             let mut current_pass = false;
-            for _k in 0..50 {
+            for _k in 0..(length / 2) {
                 // if the value in the current box and that of the current prisoner are the same,
                 // that means that the prisoner has passed
                 if current_box_value == current_prisoner {
